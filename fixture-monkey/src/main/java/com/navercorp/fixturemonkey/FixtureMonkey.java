337486c1f56273e4214980ee9de562931f6f382a
이명현[Plasma] <mhyeon.lee@navercorp.com>
2021-01-05 13:31:57 +0900

    Add giveMe method with Arbitrary (#6)
    Reviewed by: @sanghyuk-jung @ilkyun-lim @i-am-hyunjin-choi @san-ha-lee @ah-jo @jeong-min-yu @juhyeon-k @taeik-lim

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/FixtureMonkey.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/FixtureMonkey.java
@@ -5,19 +5,30 @@ import static java.util.stream.Collectors.*;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class FixtureMonkey {
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.TooManyFilterMissesException;
 
+public class FixtureMonkey {
 	public <T> Stream<T> giveMe(Class<T> type) {
 		return this.giveMe(type, true);
 	}
 
-	public <T> List<T> giveMe(Class<T> type, int size) {
-		return this.giveMe(type, size, true);
-	}
-
 	// TODO: implementation
 	public <T> Stream<T> giveMe(Class<T> type, boolean validOnly) {
+		// TODO: type to Arbitrary with generator
 		return Stream.empty();
+	}
+
+	public <T> Stream<T> giveMe(Arbitrary<T> arbitrary) {
+		return this.giveMe(arbitrary, true);
+	}
+
+	public <T> Stream<T> giveMe(Arbitrary<T> arbitrary, boolean validOnly) {
+		return this.fixtures(arbitrary, validOnly);
+	}
+
+	public <T> List<T> giveMe(Class<T> type, int size) {
+		return this.giveMe(type, size, true);
 	}
 
 	public <T> List<T> giveMe(Class<T> type, int size, boolean validOnly) {
@@ -26,12 +37,38 @@ public class FixtureMonkey {
 			.collect(toList());
 	}
 
+	public <T> List<T> giveMe(Arbitrary<T> arbitrary, int size) {
+		return this.giveMe(arbitrary, size, true);
+	}
+
+	public <T> List<T> giveMe(Arbitrary<T> arbitrary, int size, boolean validOnly) {
+		return this.giveMe(arbitrary, validOnly)
+			.limit(size)
+			.collect(toList());
+	}
+
 	public <T> T giveMeOne(Class<T> type) {
 		return this.giveMeOne(type, true);
 	}
 
-	// TODO: implementation
 	public <T> T giveMeOne(Class<T> type, boolean validOnly) {
-		return null;
+		return this.giveMe(type, 1, validOnly).get(0);
+	}
+
+	public <T> T giveMeOne(Arbitrary<T> arbitrary) {
+		return this.giveMeOne(arbitrary, true);
+	}
+
+	public <T> T giveMeOne(Arbitrary<T> arbitrary, boolean validOnly) {
+		return this.giveMe(arbitrary, 1, validOnly).get(0);
+	}
+
+	private <T> Stream<T> fixtures(Arbitrary<T> arbitrary, boolean validOnly) {
+		try {
+			return arbitrary.sampleStream();	// TODO: filter with validator
+		} catch (TooManyFilterMissesException ex) {
+			// TODO: log error message with constraint violation messages.
+			throw ex;
+		}
 	}
 }
--- fixture-monkey/src/test/java/com/navercorp/fixturemonkey/FixtureMonkeyTest.java
+++ fixture-monkey/src/test/java/com/navercorp/fixturemonkey/FixtureMonkeyTest.java
@@ -1,9 +1,19 @@
 package com.navercorp.fixturemonkey;
 
+import static org.assertj.core.api.BDDAssertions.*;
+
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.Assume;
+import net.jqwik.api.Combinators;
 import net.jqwik.api.Example;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
 
 import lombok.Builder;
 import lombok.Value;
@@ -13,14 +23,62 @@ class FixtureMonkeyTest {
 
 	@Example
 	void giveMe() {
+		Assume.that(false);
 		Stream<Person> actual = this.monkey.giveMe(Person.class);
 		// TODO: assertion
 	}
 
 	@Example
 	void giveMeOne() {
+		Assume.that(false);
 		Person actual = this.monkey.giveMeOne(Person.class);
 		// TODO: assertion
+	}
+
+	@Property
+	void giveMeWithArbitrary(@ForAll @IntRange(min = 1, max = 5000) int size) {
+		// given
+		Arbitrary<Banana> banana = new BananaArbitrary().arbitrary();
+
+		// when
+		Stream<Banana> actual = this.monkey.giveMe(banana);
+
+		// then
+		actual.limit(size).forEach(it -> {
+			then(it.color).isIn("GREEN", "YELLOW", "BROWN", "BLACK");
+			then(it.length).isBetween(3, 50);
+			then(it.rotten).isNotNull();
+		});
+	}
+
+	@Property
+	void giveMeWithArbitrarySize(@ForAll @IntRange(min = 1, max = 5000) int size) {
+		// given
+		Arbitrary<Banana> banana = new BananaArbitrary().arbitrary();
+
+		// when
+		List<Banana> actual = this.monkey.giveMe(banana, size);
+
+		// then
+		actual.forEach(it -> {
+			then(it.color).isIn("GREEN", "YELLOW", "BROWN", "BLACK");
+			then(it.length).isBetween(3, 50);
+			then(it.rotten).isNotNull();
+		});
+	}
+
+	@Property
+	void giveMeOneArbitrary() {
+		// given
+		Arbitrary<Banana> banana = new BananaArbitrary().arbitrary();
+
+		// when
+		Banana actual = this.monkey.giveMeOne(banana);
+
+		// then
+		then(actual.color).isIn("GREEN", "YELLOW", "BROWN", "BLACK");
+		then(actual.length).isBetween(3, 50);
+		then(actual.rotten).isNotNull();
 	}
 
 	@Value
@@ -35,5 +93,29 @@ class FixtureMonkeyTest {
 		int age;
 
 		String email;
+	}
+
+	@Value
+	@Builder
+	public static class Banana {
+		String color;
+		int length;
+		Boolean rotten;
+	}
+
+	public static class BananaArbitrary {
+		private final Arbitrary<String> color = Arbitraries.of(
+			"GREEN", "YELLOW", "BROWN", "BLACK"
+		);
+		private final Arbitrary<Integer> length = Arbitraries.integers().between(3, 50);
+		private final Arbitrary<Boolean> rotten = Arbitraries.forType(Boolean.class);
+
+		public Arbitrary<Banana> arbitrary() {
+			return Combinators.withBuilder(Banana::builder)
+				.use(this.color).in(Banana.BananaBuilder::color)
+				.use(this.length).in(Banana.BananaBuilder::length)
+				.use(this.rotten).in(Banana.BananaBuilder::rotten)
+				.build(Banana.BananaBuilder::build);
+		}
 	}
 }
