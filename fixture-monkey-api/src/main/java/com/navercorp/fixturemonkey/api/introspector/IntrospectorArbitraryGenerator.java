783cb88b4b70c94e542c239cdcb956d1239460ce
mhyeon-lee <mhyeon.lee@navercorp.com>
2021-12-04 15:08:22 +0900

    Introduce IntrospectorArbitraryGenerator interfaces

---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/introspector/IntrospectorArbitraryGenerator.java
@@ -0,0 +1,56 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.introspector;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+import net.jqwik.api.arbitraries.BigDecimalArbitrary;
+import net.jqwik.api.arbitraries.BigIntegerArbitrary;
+import net.jqwik.api.arbitraries.ByteArbitrary;
+import net.jqwik.api.arbitraries.CharacterArbitrary;
+import net.jqwik.api.arbitraries.DoubleArbitrary;
+import net.jqwik.api.arbitraries.FloatArbitrary;
+import net.jqwik.api.arbitraries.IntegerArbitrary;
+import net.jqwik.api.arbitraries.LongArbitrary;
+import net.jqwik.api.arbitraries.ShortArbitrary;
+import net.jqwik.api.arbitraries.StringArbitrary;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
+public interface IntrospectorArbitraryGenerator {
+	StringArbitrary strings();
+
+	CharacterArbitrary characters();
+
+	ShortArbitrary shorts();
+
+	ByteArbitrary bytes();
+
+	DoubleArbitrary doubles();
+
+	FloatArbitrary floats();
+
+	IntegerArbitrary integers();
+
+	LongArbitrary longs();
+
+	BigIntegerArbitrary bigIntegers();
+
+	BigDecimalArbitrary bigDecimals();
+}
---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/introspector/IntrospectorTimeArbitraryGenerator.java
@@ -0,0 +1,65 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.introspector;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+import net.jqwik.time.api.arbitraries.CalendarArbitrary;
+import net.jqwik.time.api.arbitraries.DateArbitrary;
+import net.jqwik.time.api.arbitraries.InstantArbitrary;
+import net.jqwik.time.api.arbitraries.LocalDateArbitrary;
+import net.jqwik.time.api.arbitraries.LocalDateTimeArbitrary;
+import net.jqwik.time.api.arbitraries.LocalTimeArbitrary;
+import net.jqwik.time.api.arbitraries.MonthDayArbitrary;
+import net.jqwik.time.api.arbitraries.OffsetDateTimeArbitrary;
+import net.jqwik.time.api.arbitraries.OffsetTimeArbitrary;
+import net.jqwik.time.api.arbitraries.PeriodArbitrary;
+import net.jqwik.time.api.arbitraries.YearArbitrary;
+import net.jqwik.time.api.arbitraries.YearMonthArbitrary;
+import net.jqwik.time.api.arbitraries.ZoneOffsetArbitrary;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
+public interface IntrospectorTimeArbitraryGenerator {
+	CalendarArbitrary calendars();
+
+	DateArbitrary dates();
+
+	InstantArbitrary instants();
+
+	LocalDateArbitrary localDates();
+
+	LocalDateTimeArbitrary localDateTimes();
+
+	LocalTimeArbitrary localTimes();
+
+	MonthDayArbitrary monthDays();
+
+	OffsetDateTimeArbitrary offsetDateTimes();
+
+	OffsetTimeArbitrary offsetTimes();
+
+	PeriodArbitrary periods();
+
+	YearArbitrary years();
+
+	YearMonthArbitrary yearMonths();
+
+	ZoneOffsetArbitrary zonOffsets();
+}
