85b038a4a461cced74f21c422c625778012c7690
mhyeon-lee <mhyeon.lee@navercorp.com>
2022-01-10 14:05:11 +0900

    Add javax validation module for annotation generator

---
+++ fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/RegexGenerator.java
@@ -0,0 +1,100 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.javax.validation.introspector;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.Pattern;
+
+import com.mifmif.common.regex.Generex;
+
+import dk.brics.automaton.RegExp;
+
+final class RegexGenerator {
+	private static final Map<String, String> PREDEFINED_CHARACTER_CLASSES;
+
+	static {
+		Map<String, String> characterClasses = new HashMap<>();
+		characterClasses.put("\\\\d", "[0-9]");
+		characterClasses.put("\\\\D", "[^0-9]");
+		characterClasses.put("\\\\s", "[ \t\n\f\r]");
+		characterClasses.put("\\\\S", "[^ \t\n\f\r]");
+		characterClasses.put("\\\\w", "[a-zA-Z_0-9]");
+		characterClasses.put("\\\\W", "[^a-zA-Z_0-9]");
+		PREDEFINED_CHARACTER_CLASSES = Collections.unmodifiableMap(characterClasses);
+	}
+
+	public List<String> generateAll(Pattern pattern) {
+		return this.generateAll(pattern, null, null);
+	}
+
+	public List<String> generateAll(Pattern pattern, @Nullable Integer min, @Nullable Integer max) {
+		String regex = pattern.regexp();
+		for (Map.Entry<String, String> charClass : PREDEFINED_CHARACTER_CLASSES.entrySet()) {
+			regex = regex.replaceAll(charClass.getKey(), charClass.getValue());
+		}
+
+		RegExp regExp;
+		Pattern.Flag[] flags = pattern.flags();
+		if (flags.length == 0) {
+			regExp = new RegExp(regex);
+		} else {
+			int intFlag = 0;
+			for (Pattern.Flag flag : flags) {
+				intFlag = intFlag | flag.getValue();
+			}
+			regExp = new RegExp(regex, intFlag);
+		}
+
+		Generex generex = new Generex(regExp.toAutomaton());
+		return this.generateAll(generex, min, max);
+	}
+
+	public List<String> generateAll(String regex) {
+		return this.generateAll(regex, null, null);
+	}
+
+	public List<String> generateAll(String regex, @Nullable Integer min, @Nullable Integer max) {
+		return this.generateAll(new Generex(regex), min, max);
+	}
+
+	private List<String> generateAll(Generex generex, @Nullable Integer min, @Nullable Integer max) {
+		if (min == null) {
+			min = 0;
+		}
+
+		if (max == null) {
+			max = 255;
+		}
+
+		Integer regexMin = min;
+		Integer regexMax = max;
+		List<String> result = generex.getMatchedStrings(100).stream()
+			.filter(it -> it.length() >= regexMin && it.length() <= regexMax)
+			.collect(toList());
+		Collections.shuffle(result);
+		return result;
+	}
+}
---
+++ fixture-monkey-javax-validation/src/test/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationArbitraryIntrospectorTest.java
@@ -0,0 +1,198 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.javax.validation.introspector;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenNoException;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.Property;
+import net.jqwik.api.arbitraries.StringArbitrary;
+
+import com.navercorp.fixturemonkey.api.introspector.ArbitraryIntrospectorContext;
+
+class JavaxValidationArbitraryIntrospectorTest {
+	private final JavaxValidationArbitraryIntrospector sut = new JavaxValidationArbitraryIntrospector();
+
+	@Property
+	void strings() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "str";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value).isNotNull();
+	}
+
+	@Property
+	void stringsNotBlank() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "notBlank";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value).isNotBlank();
+	}
+
+	@Property
+	void stringsNotEmpty() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "notEmpty";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value).isNotEmpty();
+	}
+
+	@Property
+	void stringsSize() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "size";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value.length()).isGreaterThanOrEqualTo(5);
+		then(value.length()).isLessThanOrEqualTo(10);
+	}
+
+	@Property
+	void digits() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "digits";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value.length()).isLessThanOrEqualTo(10);
+		thenNoException().isThrownBy(() -> Long.parseLong(value));
+	}
+
+	@Property
+	void pattern() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "pattern";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		Pattern pattern = Pattern.compile("[e-o]");
+		then(pattern.asPredicate().test(value)).isTrue();
+		for (int i = 0; i < value.length(); i++) {
+			char c = value.charAt(i);
+			then(c).isBetween('e', 'o');
+		}
+	}
+
+	@Property
+	void email() throws NoSuchFieldException {
+		// given
+		StringArbitrary stringArbitrary = Arbitraries.strings();
+		String propertyName = "email";
+		Field field = StringIntrospectorSpec.class.getDeclaredField(propertyName);
+		ArbitraryIntrospectorContext context = new ArbitraryIntrospectorContext(
+			String.class,
+			propertyName,
+			field.getAnnotatedType(),
+			Arrays.asList(field.getAnnotations()),
+			0.0
+		);
+
+		// when
+		Arbitrary<String> actual = this.sut.strings(stringArbitrary, context);
+
+		// then
+		String value = actual.sample();
+		then(value).containsOnlyOnce("@");
+	}
+}
