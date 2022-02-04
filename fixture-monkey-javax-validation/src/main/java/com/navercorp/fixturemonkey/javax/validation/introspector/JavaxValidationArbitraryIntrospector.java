cb49a300f85af6ed2bf6b7df33e5cc91caaf51eb
Myeonghyeon-Lee <mhyeon.lee@navercorp.com>
2022-02-04 19:46:13 +0900

    Implements short introspector (#148)

--- fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationArbitraryIntrospector.java
+++ fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationArbitraryIntrospector.java
@@ -25,11 +25,19 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Negative;
+import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 
 import org.apiguardian.api.API;
@@ -165,7 +173,107 @@ public class JavaxValidationArbitraryIntrospector implements ArbitraryIntrospect
 		ShortArbitrary shortArbitrary,
 		ArbitraryIntrospectorContext context
 	) {
-		throw new UnsupportedOperationException("Not implement yet.");
+		BigInteger min = null;
+		BigInteger max = null;
+
+		Optional<Digits> digits = context.findAnnotation(Digits.class);
+		if (digits.isPresent()) {
+			BigInteger value = BigInteger.ONE;
+			int integer = digits.get().integer();
+			if (integer > 1) {
+				value = BigInteger.TEN.pow(integer - 1);
+			}
+			max = value.multiply(BigInteger.TEN).subtract(BigInteger.ONE);
+			min = max.negate();
+		}
+
+		Optional<Min> minAnnotation = context.findAnnotation(Min.class);
+		if (minAnnotation.isPresent()) {
+			BigInteger minValue = minAnnotation.map(Min::value).map(BigInteger::valueOf).get();
+			if (min == null) {
+				min = minValue;
+			} else if (min.compareTo(minValue) > 0) {
+				min = minValue;
+			}
+		}
+
+		Optional<DecimalMin> decimalMinAnnotation = context.findAnnotation(DecimalMin.class);
+		if (decimalMinAnnotation.isPresent()) {
+			BigInteger decimalMin = decimalMinAnnotation
+				.map(DecimalMin::value)
+				.map(BigInteger::new)
+				.get();
+
+			if (!decimalMinAnnotation.map(DecimalMin::inclusive).get()) {
+				decimalMin = decimalMin.add(BigInteger.ONE);
+			}
+
+			if (min == null) {
+				min = decimalMin;
+			} else if (min.compareTo(decimalMin) < 0) {
+				min = decimalMin;
+			}
+		}
+
+		Optional<Max> maxAnnotation = context.findAnnotation(Max.class);
+		if (maxAnnotation.isPresent()) {
+			BigInteger maxValue = maxAnnotation.map(Max::value).map(BigInteger::valueOf).get();
+			if (max == null) {
+				max = maxValue;
+			} else if (max.compareTo(maxValue) > 0) {
+				max = maxValue;
+			}
+		}
+
+		Optional<DecimalMax> decimalMaxAnnotation = context.findAnnotation(DecimalMax.class);
+		if (decimalMaxAnnotation.isPresent()) {
+			BigInteger decimalMax = decimalMaxAnnotation
+				.map(DecimalMax::value)
+				.map(BigInteger::new)
+				.get();
+			if (!decimalMaxAnnotation.map(DecimalMax::inclusive).get()) {
+				decimalMax = decimalMax.subtract(BigInteger.ONE);
+			}
+
+			if (max == null) {
+				max = decimalMax;
+			} else if (max.compareTo(decimalMax) > 0) {
+				max = decimalMax;
+			}
+		}
+
+		if (context.findAnnotation(Negative.class).isPresent()) {
+			if (max == null || max.compareTo(BigInteger.ZERO) > 0) {
+				max = BigInteger.valueOf(-1);
+			}
+		}
+
+		if (context.findAnnotation(NegativeOrZero.class).isPresent()) {
+			if (max == null || max.compareTo(BigInteger.ZERO) > 0) {
+				max = BigInteger.ZERO;
+			}
+		}
+
+		if (context.findAnnotation(Positive.class).isPresent()) {
+			if (min == null || min.compareTo(BigInteger.ZERO) < 0) {
+				min = BigInteger.ONE;
+			}
+		}
+
+		if (context.findAnnotation(PositiveOrZero.class).isPresent()) {
+			if (min == null || min.compareTo(BigInteger.ZERO) < 0) {
+				min = BigInteger.ZERO;
+			}
+		}
+
+		if (min != null) {
+			shortArbitrary = shortArbitrary.greaterOrEqual(min.shortValueExact());
+		}
+		if (max != null) {
+			shortArbitrary = shortArbitrary.lessOrEqual(max.shortValueExact());
+		}
+
+		return shortArbitrary;
 	}
 
 	@Override
