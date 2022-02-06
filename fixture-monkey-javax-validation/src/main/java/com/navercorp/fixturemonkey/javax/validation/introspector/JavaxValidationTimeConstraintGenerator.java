c7e7dd900831d81d0207f793970c7989ccf1f753
mhyeon-lee <mhyeon.lee@navercorp.com>
2022-02-06 11:31:11 +0900

    Add license and @API annotation

--- fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationTimeConstraintGenerator.java
+++ fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationTimeConstraintGenerator.java
@@ -1,3 +1,21 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.fixturemonkey.javax.validation.introspector;
 
 import java.time.LocalDate;
@@ -9,8 +27,12 @@ import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.PastOrPresent;
 
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
 import com.navercorp.fixturemonkey.api.introspector.ArbitraryIntrospectorContext;
 
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
 public class JavaxValidationTimeConstraintGenerator {
 
 	public JavaxValidationDateTimeConstraint generateDateTimeConstraint(
