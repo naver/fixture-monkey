3c90192623cd140f204921a8b4af0532eeb5b6ad
Myeonghyeon-Lee <mhyeon.lee@navercorp.com>
2022-02-07 10:58:19 +0900

    Introduce ArbitraryGenerator interface (#155)

---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/generator/ArbitraryGeneratorContext.java
@@ -0,0 +1,61 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.generator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+import com.navercorp.fixturemonkey.api.property.Property;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
+public final class ArbitraryGeneratorContext {
+	private final ArbitraryProperty property;
+	private final List<ArbitraryProperty> children;
+	private final ArbitraryGenerator generator;
+
+	public ArbitraryGeneratorContext(
+		ArbitraryProperty property,
+		List<ArbitraryProperty> children,
+		ArbitraryGenerator generator
+	) {
+		this.property = property;
+		this.children = new ArrayList<>(children);
+		this.generator = generator;
+	}
+
+	public ArbitraryProperty getArbitraryProperty() {
+		return this.property;
+	}
+
+	public Property getProperty() {
+		return this.getArbitraryProperty().getProperty();
+	}
+
+	public List<ArbitraryProperty> getChildren() {
+		return Collections.unmodifiableList(this.children);
+	}
+
+	public ArbitraryGenerator getGenerator() {
+		return this.generator;
+	}
+}
