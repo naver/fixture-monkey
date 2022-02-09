0f93eeadce2603c5ddb5f5a0b8ea84b3f0e15ab0
Myeonghyeon-Lee <mhyeon.lee@navercorp.com>
2022-02-09 19:53:54 +0900

    Introduce Types utils getGenericsTypes (#176)

--- fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/type/Types.java
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/type/Types.java
@@ -1,10 +1,35 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.fixturemonkey.api.type;
 
 import java.lang.reflect.GenericDeclaration;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
 public class Types {
 	public static Class<?> getActualType(Type type) {
 		if (type.getClass() == Class.class) {
@@ -31,6 +56,38 @@ public class Types {
 
 		throw new UnsupportedOperationException(
 			"Unsupported Type to get actualType. type: " + type.getClass()
+		);
+	}
+
+	public static List<Type> getGenericsTypes(Type type) {
+		if (type.getClass() == Class.class) {
+			return Collections.emptyList();
+		}
+
+		if (TypeVariable.class.isAssignableFrom(type.getClass())) {
+			GenericDeclaration genericDeclaration = ((TypeVariable<?>)type).getGenericDeclaration();
+			if (genericDeclaration.getClass() == Class.class) {
+				return Collections.emptyList();
+			} else {
+				// Method? Constructor?
+				throw new UnsupportedOperationException(
+					"Unsupported TypeVariable's generationDeclaration type. type: " + genericDeclaration.getClass()
+				);
+			}
+		}
+
+		if (ParameterizedType.class.isAssignableFrom(type.getClass())) {
+			ParameterizedType parameterizedType = (ParameterizedType)type;
+			Type[] rawTypes = parameterizedType.getActualTypeArguments();
+			if (rawTypes == null) {
+				return Collections.emptyList();
+			}
+
+			return Arrays.asList(rawTypes);
+		}
+
+		throw new UnsupportedOperationException(
+			"Unsupported Type to get genericsTypes. type: " + type.getClass()
 		);
 	}
 }
--- fixture-monkey-api/src/test/java/com/navercorp/fixturemonkey/api/type/TypesTest.java
+++ fixture-monkey-api/src/test/java/com/navercorp/fixturemonkey/api/type/TypesTest.java
@@ -2,7 +2,9 @@ package com.navercorp.fixturemonkey.api.type;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -60,6 +62,110 @@ class TypesTest {
 		then(actual).isEqualTo(GenericSample.class);
 	}
 
+	@Test
+	void getGenericsTypesEmpty() {
+		// given
+		TypeReference<Sample> typeReference = new TypeReference<Sample>() {
+		};
+		Type type = typeReference.getType();
+
+		// when
+		List<Type> actual = Types.getGenericsTypes(type);
+
+		then(actual).isEmpty();
+	}
+
+	@Test
+	void getGenericsTypes() {
+		// given
+		TypeReference<GenericSample<String>> typeReference = new TypeReference<GenericSample<String>>() {
+		};
+		Type type = typeReference.getType();
+
+		// when
+		List<Type> actual = Types.getGenericsTypes(type);
+
+		then(actual).hasSize(1);
+
+		Type genericsType = actual.get(0);
+		then(genericsType).isEqualTo(String.class);
+	}
+
+	@Test
+	void getGenericsTypesComplex() {
+		// given
+		TypeReference<GenericSample<GenericSample2<String>>> typeReference =
+			new TypeReference<GenericSample<GenericSample2<String>>>() {
+			};
+		Type type = typeReference.getType();
+
+		// when
+		List<Type> actual = Types.getGenericsTypes(type);
+
+		then(actual).hasSize(1);
+
+		Type genericsType = actual.get(0);
+		then(genericsType).isInstanceOf(ParameterizedType.class);
+
+		ParameterizedType parameterizedType = (ParameterizedType)genericsType;
+		then(Types.getActualType(parameterizedType)).isEqualTo(GenericSample2.class);
+
+		List<Type> nestedGenericsTypes = Types.getGenericsTypes(genericsType);
+		then(nestedGenericsTypes).hasSize(1);
+		then(nestedGenericsTypes.get(0)).isEqualTo(String.class);
+	}
+
+	@Test
+	void getGenericsTypesBiGenericSample() {
+		// given
+		TypeReference<BiGenericSample<Integer, String>> typeReference =
+			new TypeReference<BiGenericSample<Integer, String>>() {
+			};
+		Type type = typeReference.getType();
+
+		// when
+		List<Type> actual = Types.getGenericsTypes(type);
+
+		then(actual).hasSize(2);
+		then(actual.get(0)).isEqualTo(Integer.class);
+		then(actual.get(1)).isEqualTo(String.class);
+	}
+
+	@Test
+	void getGenericsTypesBiGenericSampleComplex() {
+		// given
+		TypeReference<BiGenericSample<GenericSample<Integer>, BiGenericSample<Integer, String>>> typeReference =
+			new TypeReference<BiGenericSample<GenericSample<Integer>, BiGenericSample<Integer, String>>>() {
+			};
+		Type type = typeReference.getType();
+
+		// when
+		List<Type> actual = Types.getGenericsTypes(type);
+
+		then(actual).hasSize(2);
+
+		Type firstGenericsType = actual.get(0);
+		then(firstGenericsType).isInstanceOf(ParameterizedType.class);
+
+		ParameterizedType firstParameterizedType = (ParameterizedType)firstGenericsType;
+		then(Types.getActualType(firstParameterizedType)).isEqualTo(GenericSample.class);
+
+		List<Type> firstNestedGenericsTypes = Types.getGenericsTypes(firstParameterizedType);
+		then(firstNestedGenericsTypes).hasSize(1);
+		then(firstNestedGenericsTypes.get(0)).isEqualTo(Integer.class);
+
+		Type secondGenericsType = actual.get(1);
+		then(secondGenericsType).isInstanceOf(ParameterizedType.class);
+
+		ParameterizedType secondParameterizedType = (ParameterizedType)secondGenericsType;
+		then(Types.getActualType(secondParameterizedType)).isEqualTo(BiGenericSample.class);
+
+		List<Type> secondNestedGenericsTypes = Types.getGenericsTypes(secondParameterizedType);
+		then(secondNestedGenericsTypes).hasSize(2);
+		then(secondNestedGenericsTypes.get(0)).isEqualTo(Integer.class);
+		then(secondNestedGenericsTypes.get(1)).isEqualTo(String.class);
+	}
+
 	static class Sample {
 		private String name;
 	}
@@ -72,5 +178,17 @@ class TypesTest {
 
 	static class GenericSample2<T> {
 		private T name;
+	}
+
+	static class BiGenericSample<T, R> {
+		private BiGenericSample2<T, R> sample2;
+		private T name;
+		private R address;
+		private Sample test;
+	}
+
+	static class BiGenericSample2<T, R> {
+		private T name;
+		private R address;
 	}
 }
