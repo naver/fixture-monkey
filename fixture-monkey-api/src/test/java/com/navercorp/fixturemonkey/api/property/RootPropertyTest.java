dbba658fb5511e4f87f18d98324ad2008724710f
mhyeon-lee <mhyeon.lee@navercorp.com>
2022-02-10 16:41:24 +0900

    Fix checkstyle

--- fixture-monkey-api/src/test/java/com/navercorp/fixturemonkey/api/property/RootPropertyTest.java
+++ fixture-monkey-api/src/test/java/com/navercorp/fixturemonkey/api/property/RootPropertyTest.java
@@ -18,7 +18,7 @@
 
 package com.navercorp.fixturemonkey.api.property;
 
-import static org.assertj.core.api.BDDAssertions.*;
+import static org.assertj.core.api.BDDAssertions.then;
 
 import java.lang.reflect.AnnotatedType;
 
