3f0daf908dd12e6e36c0e41d8f998170d095cb1e
ah.jo <ah.jo@navercorp.com>
2022-03-04 15:51:58 +0900

    Refactor wildcard to generic type (#213)

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/FixtureMonkeyBuilder.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/FixtureMonkeyBuilder.java
@@ -127,9 +127,9 @@ public class FixtureMonkeyBuilder {
 		return this;
 	}
 
-	public FixtureMonkeyBuilder addAnnotatedArbitraryGenerator(
-		Class<?> clazz,
-		AnnotatedArbitraryGenerator<?> generator
+	public <T> FixtureMonkeyBuilder addAnnotatedArbitraryGenerator(
+		Class<T> clazz,
+		AnnotatedArbitraryGenerator<T> generator
 	) {
 		this.optionsBuilder.addAnnotatedArbitraryGenerator(clazz, generator);
 		return this;
