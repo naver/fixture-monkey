d21ddd2b11109136084894490b279e1b3322d0e7
ah.jo <ah.jo@navercorp.com>
2022-03-17 16:44:09 +0900

    Fix checkstyle

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/arbitrary/ArbitraryType.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/arbitrary/ArbitraryType.java
@@ -25,7 +25,6 @@ import java.lang.reflect.AnnotatedArrayType;
 import java.lang.reflect.AnnotatedParameterizedType;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
