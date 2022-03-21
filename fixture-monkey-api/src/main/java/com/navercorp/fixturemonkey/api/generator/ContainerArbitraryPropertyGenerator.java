9dc5f87bdb3dac5107ff1ef0a1ee835f89b36f91
Myeonghyeon-Lee <mhyeon.lee@navercorp.com>
2022-03-21 16:01:26 +0900

    Introduce ContainerArbitraryPropertyGenerator (#223)

---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/generator/ContainerArbitraryPropertyGenerator.java
@@ -0,0 +1,83 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.generator;
+
+import java.lang.reflect.AnnotatedType;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+import com.navercorp.fixturemonkey.api.option.GenerateOptions;
+import com.navercorp.fixturemonkey.api.property.ElementProperty;
+import com.navercorp.fixturemonkey.api.property.Property;
+import com.navercorp.fixturemonkey.api.type.Types;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
+public final class ContainerArbitraryPropertyGenerator implements ArbitraryPropertyGenerator {
+	@Override
+	public ArbitraryProperty property(ArbitraryPropertyGeneratorContext context) {
+		Property property = context.getProperty();
+
+		GenerateOptions generateOptions = context.getGenerateOptions();
+		double nullInject = 0.0;
+		if (generateOptions.isNullableContainer()) {
+			nullInject = generateOptions.getNullInject();
+		}
+
+		List<AnnotatedType> elementTypes = Types.getGenericsTypes(property.getAnnotatedType());
+		if (elementTypes.size() != 1) {
+			throw new IllegalArgumentException(
+				"List elementsTypes must be have 1 generics type for element. "
+					+ "propertyType: " + property.getType()
+					+ ", elementTypes: " + elementTypes
+			);
+		}
+
+		// TODO: extract container information from annotation
+		ArbitraryPropertyContainerInfo containerInfo = new ArbitraryPropertyContainerInfo(
+			0,
+			100
+		);
+
+		int size = containerInfo.getRandomSize();
+		AnnotatedType elementType = elementTypes.get(0);
+		List<Property> childProperties = new ArrayList<>();
+		for (int index = 0; index < size; index++) {
+			childProperties.add(
+				new ElementProperty(
+					property,
+					elementType,
+					index
+				)
+			);
+		}
+
+		return new ArbitraryProperty(
+			property,
+			context.getPropertyNameResolver(),
+			context.getPropertyValue(),
+			nullInject,
+			context.getElementIndex(),
+			childProperties,
+			containerInfo
+		);
+	}
+}
