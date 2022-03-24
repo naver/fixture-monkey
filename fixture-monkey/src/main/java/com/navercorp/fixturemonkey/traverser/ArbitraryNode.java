a955d9fec6ac2065bb66985636243960f34700af
mhyeon-lee <mhyeon.lee@navercorp.com>
2022-03-24 15:25:35 +0900

    Introduce new ArbitraryTraverser

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/traverser/ArbitraryNode.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/traverser/ArbitraryNode.java
@@ -18,6 +18,8 @@
 
 package com.navercorp.fixturemonkey.traverser;
 
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 import org.apiguardian.api.API;
@@ -28,8 +30,10 @@ import net.jqwik.api.Arbitrary;
 import com.navercorp.fixturemonkey.api.generator.ArbitraryProperty;
 
 @API(since = "0.4.0", status = Status.EXPERIMENTAL)
-final class ArbitraryNode {
+public final class ArbitraryNode {
 	private ArbitraryProperty arbitraryProperty;
+
+	private List<ArbitraryNode> children;
 
 	@Nullable
 	private Arbitrary<?> arbitrary;
@@ -42,6 +46,25 @@ final class ArbitraryNode {
 
 	private boolean reset = false;
 
+	ArbitraryNode(
+		ArbitraryProperty arbitraryProperty,
+		List<ArbitraryNode> children,
+		@Nullable Arbitrary<?> arbitrary
+	) {
+		this.arbitraryProperty = arbitraryProperty;
+		this.children = children;
+		this.arbitrary = arbitrary;
+	}
+
+	public ArbitraryProperty getArbitraryProperty() {
+		return this.arbitraryProperty;
+	}
+
+	public List<ArbitraryNode> getChildren() {
+		return this.children;
+	}
+
+	@Nullable
 	public Arbitrary<?> getArbitrary() {
 		return this.arbitrary;
 	}
---
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/traverser/ArbitraryTraverser.java
@@ -0,0 +1,77 @@
+package com.navercorp.fixturemonkey.traverser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+
+import com.navercorp.fixturemonkey.api.generator.ArbitraryProperty;
+import com.navercorp.fixturemonkey.api.generator.ArbitraryPropertyGenerator;
+import com.navercorp.fixturemonkey.api.generator.ArbitraryPropertyGeneratorContext;
+import com.navercorp.fixturemonkey.api.option.GenerateOptions;
+import com.navercorp.fixturemonkey.api.property.Property;
+import com.navercorp.fixturemonkey.api.property.RootProperty;
+
+@API(since = "0.4.0", status = Status.EXPERIMENTAL)
+public final class ArbitraryTraverser {
+	private final GenerateOptions generateOptions;
+
+	public ArbitraryTraverser(GenerateOptions generateOptions) {
+		this.generateOptions = generateOptions;
+	}
+
+	public ArbitraryNode traverse(RootProperty rootProperty) {
+		ArbitraryPropertyGenerator arbitraryPropertyGenerator =
+			this.generateOptions.getArbitraryPropertyGenerator(rootProperty);
+
+		ArbitraryProperty rootArbitraryProperty = arbitraryPropertyGenerator.property(
+			new ArbitraryPropertyGeneratorContext(
+				rootProperty,
+				null,
+				null,
+				null,
+				this.generateOptions
+			)
+		);
+
+		return this.traverse(rootArbitraryProperty);
+	}
+
+	private ArbitraryNode traverse(ArbitraryProperty arbitraryProperty) {
+		List<ArbitraryNode> children = new ArrayList<>();
+
+		List<Property> childProperties = arbitraryProperty.getChildProperties();
+		for (int index = 0; index < childProperties.size(); index++) {
+			Property childProperty = childProperties.get(0);
+			ArbitraryPropertyGenerator arbitraryPropertyGenerator =
+				this.generateOptions.getArbitraryPropertyGenerator(childProperty);
+			ArbitraryProperty childArbitraryProperty = arbitraryPropertyGenerator.property(
+				new ArbitraryPropertyGeneratorContext(
+					childProperty,
+					null,
+					arbitraryProperty.getContainerInfo() != null ? index : null,
+					arbitraryProperty,
+					this.generateOptions
+				)
+			);
+
+			ArbitraryNode childNode = this.traverse(childArbitraryProperty);
+			children.add(childNode);
+		}
+
+		Arbitrary<?> arbitrary = null;
+		if (arbitraryProperty.getPropertyValue() != null) {
+			arbitrary = Arbitraries.ofSuppliers(() -> arbitraryProperty.getPropertyValue().get());
+		}
+
+		return new ArbitraryNode(
+			arbitraryProperty,
+			children,
+			arbitrary
+		);
+	}
+}
