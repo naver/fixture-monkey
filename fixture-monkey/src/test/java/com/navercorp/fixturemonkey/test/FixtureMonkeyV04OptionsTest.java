778d00207175f49de2c2159c07a21da163aea221
ah.jo <ah.jo@navercorp.com>
2022-08-13 18:21:38 +0900

    Fix checkstyle

--- fixture-monkey/src/test/java/com/navercorp/fixturemonkey/test/FixtureMonkeyV04OptionsTest.java
+++ fixture-monkey/src/test/java/com/navercorp/fixturemonkey/test/FixtureMonkeyV04OptionsTest.java
@@ -30,7 +30,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import javax.annotation.Nullable;
