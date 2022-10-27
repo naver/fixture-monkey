3818ce858ea53135e984664a78d93ea9218dad57
ah.jo <ah.jo@navercorp.com>
2022-10-27 17:10:18 +0900

    Remove tests

--- fixture-monkey-jackson/src/test/java/com/navercorp/fixturemonkey/test/FixtureMonkeyV04JacksonTest.java
+++ fixture-monkey-jackson/src/test/java/com/navercorp/fixturemonkey/test/FixtureMonkeyV04JacksonTest.java
@@ -9,16 +9,11 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.List;
 
 import net.jqwik.api.Property;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import lombok.Value;
 
@@ -33,31 +28,6 @@ class FixtureMonkeyV04JacksonTest {
 	@Property
 	void jsonFormat() {
 		thenNoException().isThrownBy(() -> SUT.giveMeOne(JsonFormatSpec.class));
-	}
-
-	@Property
-	void jsonTypeInfo() {
-		thenNoException().isThrownBy(() -> SUT.giveMeOne(JsonTypeInfoIdNameSpec.class));
-	}
-
-	@Property
-	void jsonTypeListInfo() {
-		thenNoException().isThrownBy(() -> SUT.giveMeOne(JsonTypeInfoListSpec.class));
-	}
-
-	@Property
-	void jsonTypeInfoWithIdClass() {
-		thenNoException().isThrownBy(() -> SUT.giveMeOne(JsonTypeInfoIdClassSpec.class));
-	}
-
-	@Property
-	void jsonTypeWithoutAnnotations() {
-		thenNoException().isThrownBy(() -> SUT.giveMeOne(TypeWithAnnotationsSpec.class));
-	}
-
-	@Property
-	void jsonTypeListWithoutAnnotations() {
-		thenNoException().isThrownBy(() -> SUT.giveMeBuilder(TypeWithAnnotationsListSpec.class));
 	}
 
 	@Value
@@ -85,86 +55,6 @@ class FixtureMonkeyV04JacksonTest {
 
 		@JsonFormat(pattern = "yyyy MM dd HH:mm:ssZ")
 		OffsetDateTime offsetDateTime;
-	}
-
-	@Value
-	public static class JsonTypeInfoIdNameSpec {
-		@JsonTypeInfo(use = Id.NAME)
-		@JsonSubTypes({
-			@JsonSubTypes.Type(value = TypeA.class, name = "TypeA"),
-			@JsonSubTypes.Type(value = TypeB.class, name = "typeB")
-		})
-		Type type;
-	}
-
-	@Value
-	public static class JsonTypeInfoIdClassSpec {
-		@JsonTypeInfo(use = Id.CLASS)
-		@JsonSubTypes(
-			{
-				@JsonSubTypes.Type(
-					value = TypeA.class,
-					name = "com.navercorp.fixturemonkey.test.FixtureMonkeyV04Test$TypeA"
-				),
-				@JsonSubTypes.Type(
-					value = TypeB.class,
-					name = "com.navercorp.fixturemonkey.test.FixtureMonkeyV04Test$TypeB"
-				)
-			}
-		)
-		Type type;
-	}
-
-	@Value
-	public static class TypeWithAnnotationsSpec {
-		TypeWithAnnotations type;
-	}
-
-	@Value
-	public static class TypeWithAnnotationsListSpec {
-		List<TypeWithAnnotations> types;
-	}
-
-	@Value
-	public static class JsonTypeInfoListSpec {
-		@JsonTypeInfo(use = Id.NAME)
-		@JsonSubTypes({
-			@JsonSubTypes.Type(value = TypeA.class, name = "TypeA"),
-			@JsonSubTypes.Type(value = TypeB.class, name = "typeB")
-		})
-		List<Type> types;
-	}
-
-	public interface Type {
-	}
-
-	@Value
-	public static class TypeA implements Type {
-		String value;
-	}
-
-	@Value
-	@JsonTypeName("typeB")
-	public static class TypeB implements Type {
-		int value;
-	}
-
-	@JsonTypeInfo(use = Id.NAME)
-	@JsonSubTypes({
-		@JsonSubTypes.Type(value = TypeAWithAnnotations.class, name = "TypeAWithAnnotations"),
-		@JsonSubTypes.Type(value = TypeBWithAnnotations.class, name = "TypeBWithAnnotations")
-	})
-	public interface TypeWithAnnotations {
-	}
-
-	@Value
-	public static class TypeAWithAnnotations implements TypeWithAnnotations {
-		String value;
-	}
-
-	@Value
-	public static class TypeBWithAnnotations implements TypeWithAnnotations {
-		int value;
 	}
 
 	public enum JsonEnum {
