ad677db1977abbb3609ff1556862de969b1cda1b
SooKim1110 <ohjskim@naver.com>
2022-12-23 16:22:01 +0900

    Apply code review

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/builder/ManipulatorSet.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/builder/ManipulatorSet.java
@@ -1,13 +1,35 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.fixturemonkey.builder;
 
 import java.util.List;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
 
 import com.navercorp.fixturemonkey.MonkeyManipulatorFactory;
 import com.navercorp.fixturemonkey.resolver.ArbitraryManipulator;
 import com.navercorp.fixturemonkey.resolver.ContainerInfoManipulator;
 import com.navercorp.fixturemonkey.resolver.NodeResolver;
 
-public class ManipulatorSet {
+@API(since = "0.4.10", status = Status.EXPERIMENTAL)
+public final class ManipulatorSet {
 	private final List<NodeResolverObjectHolder> nodeResolverObjectHolders;
 	private final List<ContainerInfoManipulator> containerInfoManipulators;
 	private final List<ArbitraryManipulator> postConditionManipulators;
