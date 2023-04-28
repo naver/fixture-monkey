a89a0dffb2656af930e62a890f52bb4d33fe011f
Jisoo Kim <ohjskim@naver.com>
2023-04-28 13:55:16 +0900

    Add InnerSpec Javadoc (#629)

--- fixture-monkey/src/main/java/com/navercorp/fixturemonkey/customizer/InnerSpec.java
+++ fixture-monkey/src/main/java/com/navercorp/fixturemonkey/customizer/InnerSpec.java
@@ -35,6 +35,7 @@ import javax.annotation.Nullable;
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
 
+import com.navercorp.fixturemonkey.Constants;
 import com.navercorp.fixturemonkey.api.lazy.LazyArbitrary;
 import com.navercorp.fixturemonkey.customizer.InnerSpecState.ContainerInfoHolder;
 import com.navercorp.fixturemonkey.customizer.InnerSpecState.FilterHolder;
@@ -51,6 +52,16 @@ import com.navercorp.fixturemonkey.tree.NodeResolver;
 import com.navercorp.fixturemonkey.tree.NodeValuePredicate;
 import com.navercorp.fixturemonkey.tree.PropertyNameNodePredicate;
 
+/**
+ * A type-independent specification for configuring nested properties.
+ * <p>
+ * Provides methods for setting configuration for nested properties, and can be particularly
+ * useful for configuring map-type properties.
+ * </p>
+ * <p>
+ * Instances of this class can be reused to consistently and easily configure nested properties.
+ * </p>
+ */
 @SuppressWarnings("UnusedReturnValue")
 @API(since = "0.4.0", status = Status.MAINTAINED)
 public final class InnerSpec {
@@ -64,6 +75,12 @@ public final class InnerSpec {
 	private int entrySize = 0;
 	private int manipulateSize = 0;
 
+	/**
+	 * Constructs a new {@code InnerSpec} instance that refers to the root property.
+	 * <p>
+	 * Can be further configured using methods provided by the class.
+	 * </p>
+	 */
 	public InnerSpec() {
 		this(FIRST_MANIPULATOR_SEQUENCE, IdentityNodeResolver.INSTANCE, new InnerSpecState(), new ArrayList<>());
 	}
@@ -75,76 +92,152 @@ public final class InnerSpec {
 		this.innerSpecs = innerSpecs;
 	}
 
+	/**
+	 * Configures the currently referred property further
+	 * with the specifications provided inside the given {@link InnerSpec} object
+	 *
+	 * @param innerSpec An instance of {@link InnerSpec} containing the specifications
+	 *                  to be applied to the currently referred property.
+	 */
 	public InnerSpec inner(InnerSpec innerSpec) {
 		InnerSpec appendInnerSpec = newAppendNodeResolver(innerSpec, this.treePathResolver);
 		this.innerSpecs.add(appendInnerSpec);
 		return this;
 	}
 
-	public InnerSpec size(int min, int max) {
-		if (min > max) {
-			throw new IllegalArgumentException("should be min > max, min : " + min + " max : " + max);
+	/**
+	 * Sets the size of the currently referred container property.
+	 * {@code minSize} should be less than or equal to {@code maxSize}.
+	 *
+	 * @param minSize minimum size of the container to generate
+	 * @param maxSize maximum size of the container to generate
+	 */
+	public InnerSpec size(int minSize, int maxSize) {
+		if (minSize > maxSize) {
+			throw new IllegalArgumentException("should be min > max, min : " + minSize + " max : " + maxSize);
 		}
 
 		this.state.setContainerInfoHolder(
-			new ContainerInfoHolder(this.sequence + manipulateSize, this.treePathResolver, min, max)
+			new ContainerInfoHolder(this.sequence + manipulateSize, this.treePathResolver, minSize, maxSize)
 		);
 		manipulateSize++;
 		return this;
 	}
 
+	/**
+	 * Sets the size of the currently referred container property.
+	 *
+	 * @param size size of the container to generate
+	 */
 	public InnerSpec size(int size) {
 		return this.size(size, size);
 	}
 
-	public InnerSpec minSize(int min) {
-		return this.size(min, min + DEFAULT_ELEMENT_MAX_SIZE);
+	/**
+	 * Sets the size of the currently referred container property.
+	 * The size of container property would be between {@code minSize} and
+	 * {@code minSize} + {@link Constants#DEFAULT_ELEMENT_MAX_SIZE}
+	 *
+	 * @param minSize minimum size of the container to generate
+	 */
+	public InnerSpec minSize(int minSize) {
+		return this.size(minSize, minSize + DEFAULT_ELEMENT_MAX_SIZE);
 	}
 
-	public InnerSpec maxSize(int max) {
-		return this.size(Math.max(DEFAULT_ELEMENT_MIN_SIZE, max - DEFAULT_ELEMENT_MAX_SIZE), max);
+	/**
+	 * Sets the size of the currently referred container property.
+	 * The size of container property would be between
+	 * max(0, {@code maxSize} - {@link Constants#DEFAULT_ELEMENT_MAX_SIZE}) and {@code maxSize}
+	 *
+	 * @param maxSize maximum size of the container to generate
+	 */
+	public InnerSpec maxSize(int maxSize) {
+		return this.size(Math.max(DEFAULT_ELEMENT_MIN_SIZE, maxSize - DEFAULT_ELEMENT_MAX_SIZE), maxSize);
 	}
 
+	/**
+	 * Sets a key in the currently referred map property.
+	 *
+	 * @param key value of the map key to set
+	 */
 	public InnerSpec key(Object key) {
 		entrySize++;
 		setMapKey(key);
 		return this;
 	}
 
+	/**
+	 * Sets multiple keys in the currently referred map property.
+	 *
+	 * @param keys The keys to set in the map. Can be empty.
+	 */
 	public InnerSpec keys(Object... keys) {
 		Arrays.stream(keys).forEach(this::key);
 		return this;
 	}
 
+	/**
+	 * Sets a nested map key within the currently referred map property.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 the nested map key
+	 */
 	public InnerSpec key(Consumer<InnerSpec> consumer) {
 		entrySize++;
 		setMapKey(consumer);
 		return this;
 	}
 
+	/**
+	 * Sets a value in the currently referred map property.
+	 *
+	 * @param value value of the map value to set
+	 */
 	public InnerSpec value(Object value) {
 		entrySize++;
 		setMapValue(value);
 		return this;
 	}
 
+	/**
+	 * Sets multiple values in the currently referred map property.
+	 *
+	 * @param values The values to be added to the map. Can be empty.
+	 */
 	public InnerSpec values(Object... values) {
 		Arrays.stream(values).forEach(this::value);
 		return this;
 	}
 
+	/**
+	 * Sets a nested map value within the currently referred map property.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 the nested map value
+	 */
 	public InnerSpec value(Consumer<InnerSpec> consumer) {
 		entrySize++;
 		setMapValue(consumer);
 		return this;
 	}
 
+	/**
+	 * Sets an entry in the currently referred map property.
+	 *
+	 * @param key   value of the entry key to set
+	 * @param value value of the entry value to set
+	 */
 	public InnerSpec entry(Object key, @Nullable Object value) {
 		entrySize++;
 		setMapEntry(key, value);
 		return this;
 	}
 
+	/**
+	 * Sets multiple key-value pairs in the map.
+	 *
+	 * @param entries The entries to be added to the map. Should be entered in key, value order. Can be empty.
+	 */
 	public InnerSpec entries(Object... entries) {
 		if (entries.length % 2 != 0) {
 			throw new IllegalArgumentException("key-value pairs for the Map should be entered");
@@ -159,30 +252,63 @@ public final class InnerSpec {
 		return this;
 	}
 
+	/**
+	 * Sets an entry with a specified key within the currently referred map property,
+	 * and applies a consumer function to configure the value.
+	 *
+	 * @param key      value of the map key to set
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 the nested map value
+	 */
 	public InnerSpec entry(Object key, Consumer<InnerSpec> consumer) {
 		entrySize++;
 		setMapEntry(key, consumer);
 		return this;
 	}
 
-	public InnerSpec entry(Consumer<InnerSpec> consumer, @Nullable Object mapValue) {
+	/**
+	 * Sets an entry with a specified value within the currently referred map property,
+	 * and applies a consumer function to configure the key.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 the nested map key
+	 * @param value    value of the map value to set
+	 */
+	public InnerSpec entry(Consumer<InnerSpec> consumer, @Nullable Object value) {
 		entrySize++;
-		setMapEntry(consumer, mapValue);
+		setMapEntry(consumer, value);
 		return this;
 	}
 
+	/**
+	 * Sets a key in the currently referred map property with a key obtained lazily from the given supplier.
+	 *
+	 * @param supplier a supplier function that provides the value of the map key to set.
+	 */
 	public InnerSpec keyLazy(Supplier<?> supplier) {
 		entrySize++;
 		setMapKey(supplier);
 		return this;
 	}
 
+	/**
+	 * Sets a value in the currently referred map property with a value obtained lazily from the given supplier.
+	 *
+	 * @param supplier a supplier function that provides the value of the map value to set.
+	 */
 	public InnerSpec valueLazy(Supplier<?> supplier) {
 		entrySize++;
 		setMapValue(supplier);
 		return this;
 	}
 
+	/**
+	 * Sets an entry in the currently referred map property with a key and value
+	 * obtained lazily from the given suppliers.
+	 *
+	 * @param keySupplier a supplier function that provides the value of the map key to set.
+	 * @param valueSupplier a function that provides the value of the map value to set.
+	 */
 	public InnerSpec entryLazy(Supplier<?> keySupplier, Supplier<?> valueSupplier) {
 		entrySize++;
 
@@ -193,33 +319,67 @@ public final class InnerSpec {
 		return this;
 	}
 
+	/**
+	 * Sets every key in the currently referred map property using a consumer function.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 each key in the map property.
+	 */
 	public InnerSpec allKey(Consumer<InnerSpec> consumer) {
 		setMapAllKey(consumer);
 		return this;
 	}
 
+	/**
+	 * Sets every key in the currently referred map property with a key obtained lazily from the given supplier.
+	 *
+	 * @param supplier a supplier function that provides the value of the map keys to set.
+	 */
 	public InnerSpec allKeyLazy(Supplier<?> supplier) {
 		LazyArbitrary<?> lazyArbitrary = LazyArbitrary.lazy(supplier);
 		setMapAllKey(lazyArbitrary);
 		return this;
 	}
 
+	/**
+	 * Sets every value in the currently referred map property.
+	 *
+	 * @param value value of the map value to set
+	 */
 	public InnerSpec allValue(@Nullable Object value) {
 		setMapAllValue(value);
 		return this;
 	}
 
+	/**
+	 * Sets every value in the currently referred map property using a consumer function.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 each value in the map property.
+	 */
 	public InnerSpec allValue(Consumer<InnerSpec> consumer) {
 		setMapAllValue(consumer);
 		return this;
 	}
 
+	/**
+	 * Sets every value in the currently referred map property with a value obtained lazily from the given supplier.
+	 *
+	 * @param supplier a supplier function that provides the value of the map values to set.
+	 */
 	public InnerSpec allValueLazy(Supplier<?> supplier) {
 		LazyArbitrary<?> lazyArbitrary = LazyArbitrary.lazy(supplier);
 		setMapAllValue(lazyArbitrary);
 		return this;
 	}
 
+	/**
+	 * Sets every entry in the currently referred map property with a key
+	 * obtained lazily from the given supplier and a specified value.
+	 *
+	 * @param keySupplier a supplier function that provides the value of the map keys to set.
+	 * @param value       the value to set
+	 */
 	public InnerSpec allEntry(Supplier<?> keySupplier, Object value) {
 		LazyArbitrary<?> keyLazyArbitrary = LazyArbitrary.lazy(keySupplier);
 
@@ -228,38 +388,91 @@ public final class InnerSpec {
 		return this;
 	}
 
+	/**
+	 * Sets every entry in the currently referred map property with a key and value
+	 * obtained lazily from the given suppliers.
+	 *
+	 * @param keySupplier   a supplier function that provides the value of the map keys to set.
+	 * @param valueSupplier a supplier function that provides the value of the map values to set.
+	 */
 	public InnerSpec allEntryLazy(Supplier<?> keySupplier, Supplier<?> valueSupplier) {
 		return this.allEntry(keySupplier, valueSupplier);
 	}
 
+	/**
+	 * Sets an element at the specified index within the currently referred container property.
+	 *
+	 * @param index index of the element to set
+	 * @param value value of the element to set
+	 */
 	public InnerSpec listElement(int index, @Nullable Object value) {
 		setListElement(index, value);
 		return this;
 	}
 
+	/**
+	 * Sets an element at the specified index within the currently referred container property
+	 * using a consumer function.
+	 *
+	 * @param index    index of the element to set
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 the element.
+	 */
 	public InnerSpec listElement(int index, Consumer<InnerSpec> consumer) {
 		setListElement(index, consumer);
 		return this;
 	}
 
+	/**
+	 * Sets every element within the currently referred container property.
+	 *
+	 * @param value    value of the elements to set
+	 */
 	public InnerSpec allListElement(@Nullable Object value) {
 		return listElement(NO_OR_ALL_INDEX_INTEGER_VALUE, value);
 	}
 
+	/**
+	 * Sets every element within the currently referred container property using a consumer function.
+	 *
+	 * @param consumer a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                 each element.
+	 */
 	public InnerSpec allListElement(Consumer<InnerSpec> consumer) {
 		return listElement(NO_OR_ALL_INDEX_INTEGER_VALUE, consumer);
 	}
 
+	/**
+	 * Sets a property within the currently referred property.
+	 *
+	 * @param propertyName name of the property to set
+	 *                     (only string-formatted property names are allowed, and expressions are not supported)
+	 * @param value        value of the property to set
+	 */
 	public InnerSpec property(String propertyName, @Nullable Object value) {
 		setPropertyValue(propertyName, value);
 		return this;
 	}
 
+	/**
+	 * Sets a property within the currently referred property using a consumer function.
+	 *
+	 * @param propertyName name of the property to set
+	 *                     (only string-formatted property names are allowed, and expressions are not supported)
+	 * @param consumer     a consumer function that takes an {@code InnerSpec} instance as an argument and configures
+	 *                     the nested property.
+	 */
 	public InnerSpec property(String propertyName, Consumer<InnerSpec> consumer) {
 		setPropertyValue(propertyName, consumer);
 		return this;
 	}
 
+	/**
+	 * Sets the post-condition for the currently referred property.
+	 *
+	 * @param type         type of the property to set
+	 * @param filter       a predicate function that determines the post-condition of the property
+	 */
 	public <T> InnerSpec postCondition(Class<T> type, Predicate<T> filter) {
 		this.state.setFilterHolder(new FilterHolder(manipulateSize++, this.treePathResolver, type, filter));
 		return this;
