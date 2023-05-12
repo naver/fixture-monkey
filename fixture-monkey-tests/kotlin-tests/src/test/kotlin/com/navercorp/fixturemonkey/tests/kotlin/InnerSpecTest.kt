6d4b667914c86947b9e16d9148c992007dce656d
Taeik Lim <sibera21@gmail.com>
2023-05-12 12:31:05 +0900

    Add setInner method takes InnerSpec consumer
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

---
+++ fixture-monkey-tests/kotlin-tests/src/test/kotlin/com/navercorp/fixturemonkey/tests/kotlin/InnerSpecTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.tests.kotlin
+
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+import com.navercorp.fixturemonkey.kotlin.setInner
+import com.navercorp.fixturemonkey.tests.TestEnvironment.TEST_COUNT
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.RepeatedTest
+
+class InnerSpecTest {
+
+    @RepeatedTest(TEST_COUNT)
+    fun setInnerSpecByTrailingLambda() {
+        val actual = SUT.giveMeBuilder<Map<String, String>>()
+            .setInner {
+                it.keys("key1", "key2")
+                    .minSize(3)
+            }
+            .sample()
+
+        then(actual).containsKeys("key1", "key2")
+    }
+
+    companion object {
+        private val SUT: FixtureMonkey = FixtureMonkey.builder()
+            .plugin(KotlinPlugin())
+            .build()
+    }
+}
