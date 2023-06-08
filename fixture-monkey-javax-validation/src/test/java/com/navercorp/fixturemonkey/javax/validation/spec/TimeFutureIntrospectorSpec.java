c7c5e3fe09b13da1ea1289c1f94a723d89ebdb5a
ah.jo <ah.jo@navercorp.com>
2023-06-08 19:03:53 +0900

    Refactor splitting time validation spec into past and future (#660)

---
+++ fixture-monkey-javax-validation/src/test/java/com/navercorp/fixturemonkey/javax/validation/spec/TimeFutureIntrospectorSpec.java
@@ -0,0 +1,96 @@
+package com.navercorp.fixturemonkey.javax.validation.spec;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.MonthDay;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TimeFutureIntrospectorSpec {
+	@Future
+	private Calendar calendarFuture;
+
+	@FutureOrPresent
+	private Calendar calendarFutureOrPresent;
+
+	@Future
+	private Date dateFuture;
+
+	@FutureOrPresent
+	private Date dateFutureOrPresent;
+
+	@Future
+	private Instant instantFuture;
+
+	@FutureOrPresent
+	private Instant instantFutureOrPresent;
+
+	@Future
+	private LocalDate localDateFuture;
+
+	@FutureOrPresent
+	private LocalDate localDateFutureOrPresent;
+
+	@Future
+	private LocalDateTime localDateTimeFuture;
+
+	@FutureOrPresent
+	private LocalDateTime localDateTimeFutureOrPresent;
+
+	@Future
+	private LocalTime localTimeFuture;
+
+	@FutureOrPresent
+	private LocalTime localTimeFutureOrPresent;
+
+	@Future
+	private ZonedDateTime zonedDateTimeFuture;
+
+	@FutureOrPresent
+	private ZonedDateTime zonedDateTimeFutureOrPresent;
+
+	@Future
+	private MonthDay monthDayFuture;
+
+	@FutureOrPresent
+	private MonthDay monthDayFutureOrPresent;
+
+	@Future
+	private OffsetDateTime offsetDateTimeFuture;
+
+	@FutureOrPresent
+	private OffsetDateTime offsetDateTimeFutureOrPresent;
+
+	@Future
+	private OffsetTime offsetTimeFuture;
+
+	@FutureOrPresent
+	private OffsetTime offsetTimeFutureOrPresent;
+
+	@Future
+	private Year yearFuture;
+
+	@FutureOrPresent
+	private Year yearFutureOrPresent;
+
+	@Future
+	private YearMonth yearMonthFuture;
+
+	@FutureOrPresent
+	private YearMonth yearMonthFutureOrPresent;
+}
