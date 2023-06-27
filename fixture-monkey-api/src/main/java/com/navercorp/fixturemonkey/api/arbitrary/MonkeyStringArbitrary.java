d5b7e03aa920ff9e52bc34d2ff7e157e80a06566
sangy515 <sangy515@mit.edu>
2023-06-27 15:28:50 +0900

    Add license, API and final

--- fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/arbitrary/MonkeyStringArbitrary.java
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/arbitrary/MonkeyStringArbitrary.java
@@ -1,6 +1,25 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.navercorp.fixturemonkey.api.arbitrary;
 
-import java.util.function.Predicate;
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
 
 import net.jqwik.api.EdgeCases;
 import net.jqwik.api.RandomDistribution;
@@ -8,7 +27,8 @@ import net.jqwik.api.RandomGenerator;
 import net.jqwik.api.arbitraries.StringArbitrary;
 import net.jqwik.engine.properties.arbitraries.DefaultStringArbitrary;
 
-public class MonkeyStringArbitrary implements StringArbitrary {
+@API(since = "0.6.0", status = Status.EXPERIMENTAL)
+public final class MonkeyStringArbitrary implements StringArbitrary {
 
 	private final StringArbitrary delegate = new DefaultStringArbitrary();
 
