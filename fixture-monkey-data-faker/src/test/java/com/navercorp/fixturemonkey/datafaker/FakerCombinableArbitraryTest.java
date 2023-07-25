b88994f8e92137774ae7c51b218ec1c0ccbefc9c
sangy515 <sangy515@mit.edu>
2023-07-25 15:42:15 +0900

    Fix module name, edit gradle files

--- fixture-monkey-data-faker/src/test/java/com/navercorp/fixturemonkey/datafaker/FakerCombinableArbitraryTest.java
+++ fixture-monkey-data-faker/src/test/java/com/navercorp/fixturemonkey/datafaker/FakerCombinableArbitraryTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.navercorp.fixturemonkey.datafaker;
 
 import static org.assertj.core.api.BDDAssertions.then;
