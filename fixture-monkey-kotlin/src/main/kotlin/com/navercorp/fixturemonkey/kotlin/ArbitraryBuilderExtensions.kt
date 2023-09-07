3d5ebd6ac40a43eb567b4a9e9f4819f12a189136
ah.jo <ah.jo@navercorp.com>
2023-09-07 19:05:11 +0900

    Add kotlin extensions (#736)

--- fixture-monkey-kotlin/src/main/kotlin/com/navercorp/fixturemonkey/kotlin/ArbitraryBuilderExtensions.kt
+++ fixture-monkey-kotlin/src/main/kotlin/com/navercorp/fixturemonkey/kotlin/ArbitraryBuilderExtensions.kt
@@ -19,6 +19,7 @@
 package com.navercorp.fixturemonkey.kotlin
 
 import com.navercorp.fixturemonkey.ArbitraryBuilder
+import com.navercorp.fixturemonkey.api.expression.ExpressionGenerator
 import com.navercorp.fixturemonkey.customizer.InnerSpec
 
 /**
@@ -28,3 +29,23 @@ import com.navercorp.fixturemonkey.customizer.InnerSpec
 fun <T> ArbitraryBuilder<T>.setInner(innerSpecConfigurer: ((InnerSpec) -> Unit)): ArbitraryBuilder<T> {
     return this.setInner(InnerSpec().apply(innerSpecConfigurer))
 }
+
+inline fun <reified T> ArbitraryBuilder<T>.setPostCondition(expression: String, noinline predicate: (T) -> Boolean) =
+    this.setPostCondition(expression, T::class.java, predicate)
+
+inline fun <reified T> ArbitraryBuilder<T>.setPostCondition(
+    expressionGenerator: ExpressionGenerator,
+    noinline predicate: (T) -> Boolean
+) = this.setPostCondition(expressionGenerator, T::class.java, predicate)
+
+inline fun <reified T> ArbitraryBuilder<T>.setPostCondition(
+    expression: String,
+    noinline predicate: (T) -> Boolean,
+    limit: Int
+) = this.setPostCondition(expression, T::class.java, predicate, limit)
+
+inline fun <reified T> ArbitraryBuilder<T>.setPostCondition(
+    expressionGenerator: ExpressionGenerator,
+    noinline predicate: (T) -> Boolean,
+    limit: Int
+) = this.setPostCondition(expressionGenerator, T::class.java, predicate, limit)
