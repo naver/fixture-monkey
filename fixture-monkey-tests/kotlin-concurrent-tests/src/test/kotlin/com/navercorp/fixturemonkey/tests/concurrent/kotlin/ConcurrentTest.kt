1ec3722ac04c26e473e754f3179e4462c703f4aa
ah.jo <ah.jo@navercorp.com>
2023-09-15 14:40:59 +0900

    Fix Kotlin PrimaryConstructorArbitraryIntrospector generates concurrently

---
+++ fixture-monkey-tests/kotlin-concurrent-tests/src/test/kotlin/com/navercorp/fixturemonkey/tests/concurrent/kotlin/ConcurrentTest.kt
@@ -0,0 +1,36 @@
+package com.navercorp.fixturemonkey.tests.concurrent.kotlin
+
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+import com.navercorp.fixturemonkey.kotlin.giveMeOne
+import com.navercorp.fixturemonkey.tests.TestEnvironment
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.RepeatedTest
+
+class ConcurrentTest {
+    @RepeatedTest(TestEnvironment.TEST_COUNT)
+    fun test1() {
+        val actual: KotlinObject = SUT.giveMeOne<KotlinObject>()
+        then(actual).isNotNull()
+    }
+
+    @RepeatedTest(TestEnvironment.TEST_COUNT)
+    fun test2() {
+        val actual: KotlinObject = SUT.giveMeOne<KotlinObject>()
+        then(actual).isNotNull()
+    }
+
+    @RepeatedTest(TestEnvironment.TEST_COUNT)
+    fun test3() {
+        val actual: KotlinObject = SUT.giveMeOne<KotlinObject>()
+        then(actual).isNotNull()
+    }
+
+    data class KotlinObject(val value: String, val map: Map<String, String>)
+
+    companion object {
+        private val SUT = FixtureMonkey.builder()
+            .plugin(KotlinPlugin())
+            .build()
+    }
+}
