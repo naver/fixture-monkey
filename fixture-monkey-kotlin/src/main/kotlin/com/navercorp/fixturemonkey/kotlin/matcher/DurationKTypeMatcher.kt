998508d349737aa63b363aacd419578383a10a39
geonc123 <92219795+this-is-spear@users.noreply.github.com>
2024-03-14 22:49:05 +0900

    refactor : modify match method

--- fixture-monkey-kotlin/src/main/kotlin/com/navercorp/fixturemonkey/kotlin/matcher/DurationKTypeMatcher.kt
+++ fixture-monkey-kotlin/src/main/kotlin/com/navercorp/fixturemonkey/kotlin/matcher/DurationKTypeMatcher.kt
@@ -20,11 +20,11 @@ package com.navercorp.fixturemonkey.kotlin.matcher
 
 import com.navercorp.fixturemonkey.api.matcher.Matcher
 import com.navercorp.fixturemonkey.api.property.Property
-import com.navercorp.fixturemonkey.api.type.Types
+import com.navercorp.fixturemonkey.kotlin.type.actualType
 import kotlin.time.Duration
 
 class DurationKTypeMatcher : Matcher {
     override fun match(property: Property): Boolean {
-        return Duration::class.java.isAssignableFrom(Types.getActualType(property.type))
+        return Duration::class.java.isAssignableFrom(property.type.actualType())
     }
 }
