1deae73bfe14da616aa4a12d9c53ddf8ba8b84b0
ctmanjak <ctmanjak@gmail.com>
2024-06-18 20:06:28 +0900

    Fix checkstyle.

--- fixture-monkey-tests/java-17-tests/src/test/java/com/navercorp/fixturemonkey/tests/java17/JdkVariantOptionsTest.java
+++ fixture-monkey-tests/java-17-tests/src/test/java/com/navercorp/fixturemonkey/tests/java17/JdkVariantOptionsTest.java
@@ -15,21 +15,21 @@ import com.navercorp.fixturemonkey.tests.java17.SealedClassTestSpecs.BaseSealedC
 import com.navercorp.fixturemonkey.tests.java17.SealedClassTestSpecs.SealedClass;
 
 class JdkVariantOptionsTest {
-    @RepeatedTest(TEST_COUNT)
+	@RepeatedTest(TEST_COUNT)
 	@DisplayName("User-added ObjectPropertyGenerators have a higher priority.")
 	void sampleSealedClass() {
 		// given
 		FixtureMonkey sut = FixtureMonkey.builder()
-		    .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
-		    .defaultNotNull(true)
-		    .pushAssignableTypeObjectPropertyGenerator(
-			    BaseSealedClass.class,
-			    new InterfaceObjectPropertyGenerator<>(
-			    	List.of(SealedClass.class)
-			    )
-		    )
-		    .build();
-		
+			.objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+			.defaultNotNull(true)
+			.pushAssignableTypeObjectPropertyGenerator(
+				BaseSealedClass.class,
+				new InterfaceObjectPropertyGenerator<>(
+					List.of(SealedClass.class)
+				)
+			)
+			.build();
+
 		// when
 		BaseSealedClass actual = sut.giveMeOne(BaseSealedClass.class);
 
