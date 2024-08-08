713a33ea4596d246b909664b89385eb3be973153
sejin park <seijin0722@naver.com>
2024-08-08 16:54:29 +0900

    refactor : seed annotation and using Randoms

---
+++ fixture-monkey-junit-jupiter/src/main/java/com/navercorp/fixturemonkey/junit/jupiter/annotation/Seed.java
@@ -0,0 +1,9 @@
+package com.navercorp.fixturemonkey.junit.jupiter.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Seed {
+	long value();
+}
--- fixture-monkey-junit-jupiter/src/main/java/com/navercorp/fixturemonkey/junit/jupiter/extension/FixtureMonkeySeedExtension.java
+++ fixture-monkey-junit-jupiter/src/main/java/com/navercorp/fixturemonkey/junit/jupiter/extension/FixtureMonkeySeedExtension.java
@@ -1,31 +1,82 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.navercorp.fixturemonkey.junit.jupiter.extension;
 
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.util.logging.Logger;
 
-public class FixtureMonkeySeedExtension implements AfterTestExecutionCallback {
+import com.navercorp.fixturemonkey.api.random.Randoms;
+import com.navercorp.fixturemonkey.junit.jupiter.annotation.Seed;
 
-	private static final Logger logger = Logger.getLogger(FixtureMonkeySeedExtension.class.getName());
+public final class FixtureMonkeySeedExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+	private static final ThreadLocal<Long> SEED_HOLDER = new ThreadLocal<>();
 
-	private static final ThreadLocal<Long> seedHolder = new ThreadLocal<>();
+	private static final Logger logger = LoggerFactory.getLogger(FixtureMonkeySeedExtension.class);
+
 
 	/**
-	 * Store the seed used in the Fixture Monkey builder before test execution and output that seed if the test fails
-	 * @param seed the seed used in the Fixture Monkey builder
+	 * Sets the seed for Fixture Monkey before the test execution.
+	 * If a method is annotated with @Seed, it uses the specified seed.
+	 * Otherwise, it generates a new random seed.
+	 *
+	 * @param context the current extension context
 	 */
-	public static void setSeed(long seed) {
-		seedHolder.set(seed);
+	@Override
+	public void beforeTestExecution(ExtensionContext context) throws Exception {
+		Seed seedAnnotation = context.getRequiredTestMethod().getAnnotation(Seed.class);
+		if (seedAnnotation != null) {
+			long seed = seedAnnotation.value();
+			setSeed(seed);
+		} else {
+			long seed = Randoms.currentSeed();
+			setSeed(seed);
+		}
 	}
 
+	/**
+	 * Store the seed in the SEED_HOLDER before test execution.
+	 * @param seed the seed to be stored
+	 */
+	private void setSeed(long seed) {
+		SEED_HOLDER.set(seed);
+	}
+
+	/**
+	 * Logs the seed used for the test if the test fails.
+	 *
+	 * @param context the current extension context
+	 * @throws Exception if any error occurs during logging
+	 */
 	@Override
 	public void afterTestExecution(ExtensionContext context) throws Exception {
-		Long seed = seedHolder.get();
+		Long seed = SEED_HOLDER.get();
 		if (context.getExecutionException().isPresent()) {
 			Method testMethod = context.getRequiredTestMethod();
-			logger.info(() -> String.format("Test Method [%s] failed with seed: %d", testMethod.getName(), seed));
+			if (seed != null) {
+				logger.error(() -> String.format("Test Method [%s] failed with seed: %d", testMethod.getName(), seed));
+			} else {
+				logger.error(() -> String.format("Test Method [%s] failed, but no seed was set.", testMethod.getName()));
+			}
 		}
 	}
 }
