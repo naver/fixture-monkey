3c875029b01e480c46052415e6df93a8a8c3d492
hocaron <kkannu0407@gmail.com>
2025-07-20 23:04:23 +0900

    Merge DataFakerFieldResolver into DataFakerArbitraryIntrospector

--- fixture-monkey-datafaker/src/main/java/com/navercorp/fixturemonkey/datafaker/introspector/DataFakerArbitraryIntrospector.kt
+++ fixture-monkey-datafaker/src/main/java/com/navercorp/fixturemonkey/datafaker/introspector/DataFakerArbitraryIntrospector.kt
@@ -1,26 +1,79 @@
 package com.navercorp.fixturemonkey.datafaker.introspector
 
+import com.navercorp.fixturemonkey.api.arbitrary.CombinableArbitrary
 import com.navercorp.fixturemonkey.api.generator.ArbitraryGeneratorContext
 import com.navercorp.fixturemonkey.api.introspector.ArbitraryIntrospector
 import com.navercorp.fixturemonkey.api.introspector.ArbitraryIntrospectorResult
+import com.navercorp.fixturemonkey.api.matcher.Matcher
+import com.navercorp.fixturemonkey.api.property.Property
 import com.navercorp.fixturemonkey.api.type.Types
-import com.navercorp.fixturemonkey.datafaker.support.DataFakerFieldResolver
+import com.navercorp.fixturemonkey.datafaker.arbitrary.DataFakerStringArbitrary
 
-class DataFakerArbitraryIntrospector : ArbitraryIntrospector {
+class DataFakerArbitraryIntrospector : ArbitraryIntrospector, Matcher {
+
+    override fun match(property: Property): Boolean {
+        val fieldName = property.name ?: return false
+        val fieldType = Types.getActualType(property.type) as Class<*>? ?: return false
+
+        if (fieldType != String::class.java) return false
+
+        return isDataFakerTargetField(fieldName)
+    }
+
     override fun introspect(context: ArbitraryGeneratorContext): ArbitraryIntrospectorResult {
         val property = context.arbitraryProperty.objectProperty.property
-        val fieldName = property.name ?: return ArbitraryIntrospectorResult.NOT_INTROSPECTED
-        val fieldType = Types.getActualType(property.type) as Class<*>
 
-        if (!DataFakerFieldResolver.isFakerTargetField(fieldType, fieldName)) {
+        if (!match(property)) {
             return ArbitraryIntrospectorResult.NOT_INTROSPECTED
         }
 
+        val fieldName = property.name!!
+
         return try {
-            val combinableArbitrary = DataFakerFieldResolver.resolveFakerArbitrary(fieldType, fieldName)
+            val combinableArbitrary = resolveDataFakerArbitrary(fieldName)
             ArbitraryIntrospectorResult(combinableArbitrary)
         } catch (e: Exception) {
             ArbitraryIntrospectorResult.NOT_INTROSPECTED
         }
+    }
+
+    private fun isDataFakerTargetField(fieldName: String): Boolean {
+        val keywords = listOf(
+            "name", "firstName", "lastName", "fullName",
+            "address", "city", "email",
+            "phone", "phoneNumber",
+            "creditCard"
+        )
+        return keywords.any { fieldName.contains(it, ignoreCase = true) }
+    }
+
+    private fun resolveDataFakerArbitrary(fieldName: String): CombinableArbitrary<*> = when {
+        fieldName.contains("name", ignoreCase = true) ||
+                fieldName.contains("firstName", ignoreCase = true) ||
+                fieldName.contains("lastName", ignoreCase = true) ||
+                fieldName.contains(
+                    "fullName",
+                    ignoreCase = true
+                ) -> DataFakerStringArbitrary.name() as CombinableArbitrary<*>
+
+        fieldName.contains("address", ignoreCase = true) ||
+                fieldName.contains(
+                    "city",
+                    ignoreCase = true
+                ) -> DataFakerStringArbitrary.address() as CombinableArbitrary<*>
+
+        fieldName.contains("email", ignoreCase = true) -> DataFakerStringArbitrary.internet() as CombinableArbitrary<*>
+
+        fieldName.contains(
+            "phone",
+            ignoreCase = true
+        ) -> DataFakerStringArbitrary.phoneNumber() as CombinableArbitrary<*>
+
+        fieldName.contains(
+            "creditCard",
+            ignoreCase = true
+        ) -> DataFakerStringArbitrary.finance() as CombinableArbitrary<*>
+
+        else -> error("No DataFaker arbitrary found for field: $fieldName")
     }
 }
--- fixture-monkey-datafaker/src/main/java/com/navercorp/fixturemonkey/datafaker/plugin/DataFakerPlugin.kt
+++ fixture-monkey-datafaker/src/main/java/com/navercorp/fixturemonkey/datafaker/plugin/DataFakerPlugin.kt
@@ -1,28 +1,22 @@
 package com.navercorp.fixturemonkey.datafaker.plugin
 
-import com.navercorp.fixturemonkey.api.matcher.Matcher
 import com.navercorp.fixturemonkey.api.option.FixtureMonkeyOptionsBuilder
 import com.navercorp.fixturemonkey.api.plugin.Plugin
-import com.navercorp.fixturemonkey.api.type.Types
 import com.navercorp.fixturemonkey.datafaker.generator.DataFakerObjectPropertyGenerator
 import com.navercorp.fixturemonkey.datafaker.introspector.DataFakerArbitraryIntrospector
-import com.navercorp.fixturemonkey.datafaker.support.DataFakerFieldResolver
 
 class DataFakerPlugin : Plugin {
     override fun accept(optionsBuilder: FixtureMonkeyOptionsBuilder) {
-        val dataFakerMatcher = Matcher { property ->
-            val actualType = Types.getActualType(property.type) as Class<*>
-            DataFakerFieldResolver.isFakerTargetField(actualType, property.name)
-        }
+        val dataFakerIntrospector = DataFakerArbitraryIntrospector()
 
         optionsBuilder.insertFirstArbitraryObjectPropertyGenerator(
-            dataFakerMatcher,
+            dataFakerIntrospector,
             DataFakerObjectPropertyGenerator()
         )
         
         optionsBuilder.insertFirstArbitraryIntrospector(
-            dataFakerMatcher,
-            DataFakerArbitraryIntrospector()
+            dataFakerIntrospector,
+            dataFakerIntrospector
         )
     }
 }
