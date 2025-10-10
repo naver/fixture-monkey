b57339dea1058082ef818b9c5cbe2ed26121e5f5
kailis <tech.bin.xyz@gmail.com>
2025-10-11 04:48:10 +0900

    fix: Improve Kotest character arbitrary generator

--- fixture-monkey-kotest/src/main/kotlin/com/navercorp/fixturemonkey/kotest/KotestCharacterCombinableArbitrary.kt
+++ fixture-monkey-kotest/src/main/kotlin/com/navercorp/fixturemonkey/kotest/KotestCharacterCombinableArbitrary.kt
@@ -20,62 +20,108 @@ package com.navercorp.fixturemonkey.kotest
 
 import com.navercorp.fixturemonkey.api.arbitrary.CharacterCombinableArbitrary
 import io.kotest.property.Arb
-import io.kotest.property.arbitrary.char
+import io.kotest.property.arbitrary.choice
 import io.kotest.property.arbitrary.filter
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.of
 import io.kotest.property.arbitrary.single
 import org.apiguardian.api.API
 import org.apiguardian.api.API.Status
 import java.util.function.Predicate
 
 @API(since = "1.1.16", status = Status.EXPERIMENTAL)
-class KotestCharacterCombinableArbitrary(private val arb: Arb<Char> = Arb.char()) : CharacterCombinableArbitrary {
+class KotestCharacterCombinableArbitrary(
+    private val arb: Arb<Char> = DEFAULT_CHAR_ARB
+) : CharacterCombinableArbitrary {
     override fun combined(): Char = arb.single()
 
     override fun rawValue(): Char = this.combined()
 
     override fun withRange(min: Char, max: Char): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it in min..max })
+        KotestCharacterCombinableArbitrary(charRange(min, max))
 
     override fun alphabetic(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isLetter() })
+        KotestCharacterCombinableArbitrary(Arb.choice(UPPERCASE_RANGE_ARB, LOWERCASE_RANGE_ARB))
 
     override fun numeric(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isDigit() })
+        KotestCharacterCombinableArbitrary(NUMERIC_RANGE_ARB)
 
     override fun alphaNumeric(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isLetterOrDigit() })
+        KotestCharacterCombinableArbitrary(Arb.choice(UPPERCASE_RANGE_ARB, LOWERCASE_RANGE_ARB, NUMERIC_RANGE_ARB))
 
     override fun ascii(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.code <= 127 })
+        KotestCharacterCombinableArbitrary(charRange('\u0000', '\u007F'))
 
     override fun uppercase(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isUpperCase() })
+        KotestCharacterCombinableArbitrary(UPPERCASE_RANGE_ARB)
 
     override fun lowercase(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isLowerCase() })
+        KotestCharacterCombinableArbitrary(LOWERCASE_RANGE_ARB)
 
     override fun korean(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it in '가'..'힣' })
-
-    private fun isEmojiCodePoint(codePoint: Int): Boolean {
-        return codePoint in 0x1F600..0x1F64F || // Emoticons
-            codePoint in 0x1F300..0x1F5FF || // Misc Symbols and Pictographs
-            codePoint in 0x1F680..0x1F6FF || // Transport and Map
-            codePoint in 0x2600..0x26FF || // Misc symbols
-            codePoint in 0x2700..0x27BF // Dingbats
-    }
+        KotestCharacterCombinableArbitrary(charRange('가', '힣'))
 
     override fun emoji(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { isEmojiCodePoint(it.code) })
+        KotestCharacterCombinableArbitrary(Arb.of(EMOJI_CHAR_CANDIDATES))
 
     override fun whitespace(): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { it.isWhitespace() })
+        KotestCharacterCombinableArbitrary(Arb.of(WHITESPACE_CHAR_CANDIDATES))
 
     override fun filter(tries: Int, predicate: Predicate<Char>): CharacterCombinableArbitrary =
-        KotestCharacterCombinableArbitrary(Arb.char().filter { predicate.test(it) })
+        KotestCharacterCombinableArbitrary(arb.filter { predicate.test(it) })
 
     override fun clear() {
     }
 
     override fun fixed(): Boolean = false
+
+    companion object {
+        private val DEFAULT_CHAR_ARB: Arb<Char> = charRange(Char.MIN_VALUE, Char.MAX_VALUE)
+        private val UPPERCASE_RANGE_ARB: Arb<Char> = charRange('A', 'Z')
+        private val LOWERCASE_RANGE_ARB: Arb<Char> = charRange('a', 'z')
+        private val NUMERIC_RANGE_ARB: Arb<Char> = charRange('0', '9')
+        private val EMOJI_CHAR_CANDIDATES: List<Char> = listOf(
+            '\u263A', // ☺
+            '\u263B', // ☻
+            '\u2661', // ♡
+            '\u2665', // ♥
+            '\u2660', // ♠
+            '\u2663', // ♣
+            '\u2666', // ♦
+            '\u2600', // ☀
+            '\u2601', // ☁
+            '\u2602', // ☂
+            '\u2603', // ☃
+            '\u2604', // ☄
+            '\u2614', // ☔
+            '\u2615', // ☕
+            '\u260E', // ☎
+            '\u2618', // ☘
+            '\u2728', // ✨
+            '\u2764', // ❤
+            '\u2708', // ✈
+            '\u270C' // ✌
+        )
+        private val WHITESPACE_CHAR_CANDIDATES: List<Char> = listOf(
+            ' ',
+            '\t',
+            '\n',
+            '\r',
+            '\u000B', // vertical tab
+            '\u000C' // form feed
+        )
+
+        private fun charRange(startInclusive: Char, endInclusive: Char): Arb<Char> {
+            if (startInclusive == endInclusive) {
+                return Arb.of(startInclusive)
+            }
+            val (min, max) = if (startInclusive <= endInclusive) {
+                startInclusive to endInclusive
+            } else {
+                endInclusive to startInclusive
+            }
+            return Arb.int(min.code..max.code).map(Int::toChar)
+        }
+    }
 }
