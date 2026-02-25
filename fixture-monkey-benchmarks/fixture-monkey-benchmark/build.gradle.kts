9837685250910e7563711f4cdf1cabe5b69b735e
ah.jo <seongside@gmail.com>
2026-02-25 13:35:12 +0900

    Update benchmark setup adding quick, include

--- fixture-monkey-benchmarks/fixture-monkey-benchmark/build.gradle.kts
+++ fixture-monkey-benchmarks/fixture-monkey-benchmark/build.gradle.kts
@@ -15,4 +15,14 @@ jmh {
     warmupIterations = 3
     iterations = 10
     resultFormat = "CSV"
+
+    val includePattern = project.findProperty("jmh.include") as String?
+    if (includePattern != null) {
+        includes.set(listOf(includePattern))
+    }
+
+    if (project.hasProperty("jmh.quick")) {
+        warmupIterations = 1
+        iterations = 3
+    }
 }
